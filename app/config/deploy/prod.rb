ed5b0cfeac0a20d2c8a5148b3fb87fb0b29833f2
Jeremie Samson <jeremie.samson76@gmail.com>
2017-07-18 11:59:05 +0200

    change dev to prod

--- app/config/deploy/prod.rb
+++ app/config/deploy/prod.rb
@@ -1,7 +1,7 @@
 set :deploy_to, "/var/www/soju.ovh"
 set :domain, "ns378858.ip-5-196-69.eu"
 set :user, "jerem"
-set :symfony_env_prod, "dev"
+set :symfony_env_prod, "prod"
 
 role :web,        domain
 role :app,        domain, :primary => true
